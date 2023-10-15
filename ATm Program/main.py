73726170ec3d18a13235337d1d6c8b56abfcd4be
danangfir <danangfirmino26@gmail.com>
2023-10-15 14:55:37 +0700

    membuat kartu identitas ATM

--- ATm Program/main.py
+++ ATm Program/main.py
@@ -1,4 +1,10 @@
+# Berbagai package yang dibutuhkan
 import random
 
-nama: str = input("Masukan Nama anda :")
+# Membuat kartu identitas ATM 
+nama = input("Masukan nama anda :")
+id_number = random.random()
+password_user: int = input("Masukan Pasword anda :")
 
+# Menampilkan Data User
+print(nama,'\n', id_number, '\n', password_user)
